75b6cedcb11baa8e3362103cea0ef4362bd0ad59
sIKE23 <sike@pobox.com>
2014-03-29 00:41:26 -0500

    A bit more word smithing in actions.py

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/actions.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/actions.py
@@ -1080,9 +1080,9 @@ def castSpell(card, x = 0, y = 0):
 		#TODO Who is casting the spell?
 		infostr = ""
 		if "Enchantment" in card.Type:
-			infostr= "Printed reveal cost is {}".format(castingcosts[1])
+			infostr= "Printed reveal cost of {} is {}".format(card.Name,castingcosts[1])
 		else:
-			infostr= "Printed casting cost is {}".format(card.Cost)
+			infostr= "Printed casting cost of {} is {}".format(card.Name,card.Cost)
 		# find any discounts from equipment(School, Type, Subtype, Targetbased?)
 		discount = 0
 		for c in table:
