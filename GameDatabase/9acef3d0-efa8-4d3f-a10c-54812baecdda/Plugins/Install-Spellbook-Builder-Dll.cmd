2025da4f08ba767bf6581d0857c5a8fc68ea2d8e
sIKE23 <sike@pobox.com>
2014-04-07 19:53:12 -0500

    Script to unblock and install spellbook builder dll #62
    Decided to use a shell script instead of powershell. Test this and let
    me know what you think.

---
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+cd /d %~dp0
+
+@echo Installing the Spellbook Builder DLL for the Mage Wars on OCTGN module!
+@echo.
+@echo Making sure that file is unblocked 
+@echo.
+powershell -command unblock-file .\MWPlugIn\Octgn.Magewars.dll && @echo   - It is not Blocked.....
+
+@echo.
+@echo Making sure that the folder where we want to install the plugin exists and if it doesn't we will create it...
+@echo.
+if exist ..\..\Plugins\MWPlugIn (@echo   - Folder already exists!) else (mkdir ..\..\Plugins\MWPlugIn & @echo   - Target folder was created!) 
+@echo.
+
+@echo Copy Dll to target folder, replacing the existing dll if needed
+@echo.
+copy .\MWPlugIn\Octgn.Magewars.dll ..\..\Plugins\MWPlugIn /y >nul && @echo   - DLL file copied to folder successfully
+
+@echo.
+if exist ..\..\Plugins\MWPlugIn\Octgn.Magewars.dll (@echo Done updating the Spellbookbuilder DLL for the Mage Wars on OCTGN module!) else (@echo Something bad happened!)
+@echo.
+
+pause
