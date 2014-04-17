1ffbc2919ea3a563da209f415e8a75a1ecf81785
sIKE23 <sike@pobox.com>
2014-04-17 01:10:00 -0500

    More work on the script #62
    Had the source directory wrong

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
@@ -11,12 +11,12 @@ powershell -command unblock-file .\MWPlugIn\Octgn.Magewars.dll && @echo   - It i
 @echo.
 @echo Making sure that the folder where we want to install the plugin exists and if it doesn't we will create it...
 @echo.
-if exist ..\..\Plugins\MWPlugIn (@echo   - Folder already exists!) else (mkdir ..\..\Plugins\MWPlugIn & @echo   - Target folder was created!) 
+if exist ..\..\Plugins\Octgn.MagewarsPlugin (@echo   - Folder already exists!) else (mkdir ..\..\Plugins\MWPlugIn & @echo   - Target folder was created!) 
 @echo.
 
 @echo Copy Dll to target folder, replacing the existing dll if needed
 @echo.
-copy .\MWPlugIn\Octgn.Magewars.dll ..\..\Plugins\MWPlugIn /y >nul && @echo   - DLL file copied to folder successfully
+copy .\Octgn.MagewarsPlugin\Octgn.Magewars.dll ..\..\Plugins\MWPlugIn /y >nul && @echo   - DLL file copied to folder successfully
 
 @echo.
 if exist ..\..\Plugins\MWPlugIn\Octgn.Magewars.dll (@echo Done updating the Spellbookbuilder DLL for the Mage Wars on OCTGN module!) else (@echo Something bad happened!)
