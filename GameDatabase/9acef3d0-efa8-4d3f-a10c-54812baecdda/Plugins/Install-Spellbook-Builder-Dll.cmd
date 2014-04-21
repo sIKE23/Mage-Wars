853a8be529d4dd9d58f9f64c5bb9e6ed0a09acd3
sIKE23 <sike@pobox.com>
2014-04-20 22:31:12 -0500

    Fixed target directory
    need one more ..\ to land in the root (OCTGN) directory.

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
@@ -6,20 +6,20 @@ cd /d %~dp0
 @echo.
 @echo Making sure that file is unblocked 
 @echo.
-powershell -command unblock-file .\MWPlugIn\Octgn.Magewars.dll && @echo   - It is not Blocked.....
+powershell -command unblock-file .\Octgn.MagewarsPlugin\Octgn.Magewars.dll && @echo  - It is not Blocked.....
 
 @echo.
 @echo Making sure that the folder where we want to install the plugin exists and if it doesn't we will create it...
 @echo.
-if exist ..\..\Plugins\Octgn.MagewarsPlugin (@echo   - Folder already exists!) else (mkdir ..\..\Plugins\MWPlugIn & @echo   - Target folder was created!) 
+if exist ..\..\..\Plugins\MWPlugIn (@echo   - Folder already exists!) else (mkdir ..\..\..\Plugins\MWPlugIn & @echo   - Target folder was created!) 
 @echo.
 
-@echo Copy Dll to target folder, replacing the existing dll if needed
+@echo Copy DLL to target folder, replacing the existing DLL if needed
 @echo.
-copy .\Octgn.MagewarsPlugin\Octgn.Magewars.dll ..\..\Plugins\MWPlugIn /y >nul && @echo   - DLL file copied to folder successfully
+copy .\Octgn.MagewarsPlugin\Octgn.Magewars.dll ..\..\..\Plugins\MWPlugIn /y >nul && @echo   - DLL file copied to folder successfully
 
 @echo.
-if exist ..\..\Plugins\MWPlugIn\Octgn.Magewars.dll (@echo Done updating the Spellbookbuilder DLL for the Mage Wars on OCTGN module!) else (@echo Something bad happened!)
+if exist ..\..\..\Plugins\MWPlugIn\Octgn.Magewars.dll (@echo Done updating the Spellbookbuilder DLL for the Mage Wars on OCTGN module!) else (@echo Something bad happened!)
 @echo.
 
 pause
