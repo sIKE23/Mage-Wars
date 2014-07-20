ce738bdcda8539ba53478c195de835146f284062
sIKE23 <sike@pobox.com>
2014-07-20 16:13:32 -0500

    Rolling 1.8.0.0 - Forged in Fire!

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/changelog.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/changelog.py
@@ -1,4 +1,13 @@
 changelog = {
+    1080000: ("1.8.0.0", "20 July 2014", [
+        " Game Documents | Playing Mage Wars on OCTGN",
+        " Game Documents | OCTGN Apprentice Mode",
+        " Game Documents | Keyboard Shortcut Mappings",
+        " Game Documents | Mage Wars Complete Codex",
+        " Game Documents | Mage Wars Condition Markers",
+        " Game Documents | Spellbook Builder Install Help",
+        " Game Documents | ChangeLog",
+        ]),
     1070000: ("1.7.0.0", "01 June 2014", [
         " Game Documents | Playing Mage Wars on OCTGN",
         " Game Documents | OCTGN Apprentice Mode",
