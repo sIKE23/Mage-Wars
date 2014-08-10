b455235b320dc5ba3ef4425b836eeb4982884db2
sIKE23 <sike@pobox.com>
2014-08-10 15:13:25 -0500

    #107 - WoundedPrey Marker now shown at reveal again
    Silly name change should of caught this......

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/actions.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/actions.py
@@ -1284,7 +1284,7 @@ def flipcard(card, x = 0, y = 0):
 					card.markers[DeflectR] = 1
 			if "Beastmaster" == card.name:
 					card.markers[Pet] = 1
-			if "Beastmaster (Johktari)" == card.name:
+			if "Johktari Beastmaster ()" == card.name:
 					card.markers[WoundedPrey] = 1
 			if "Priest" == card.name:
 					card.markers[HolyAvenger] = 1
