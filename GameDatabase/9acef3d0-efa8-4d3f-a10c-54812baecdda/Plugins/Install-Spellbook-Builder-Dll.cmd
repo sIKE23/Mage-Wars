29e93291f2dccbff296b8f9faf8f7e6bd748421c
sIKE23 <sike@pobox.com>
2015-01-11 20:32:01 -0600

    #118 - Version bump for release

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/Plugins/Install-Spellbook-Builder-Dll.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem v1.10.0.0
+rem v1.11.0.0
 cd /d %~dp0
 
 @echo Please refer to the Spellbook Builder Page on GamersJudgement for information on how to install this plugin:
--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/changelog.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/changelog.py
@@ -1,4 +1,13 @@
 changelog = {
+		1110000: ("1.11.0.0", "10 January 2015", [
+			" Game Documents | Playing Mage Wars on OCTGN",
+			" Game Documents | OCTGN Apprentice Mode",
+			" Game Documents | Keyboard Shortcut Mappings",
+			" Game Documents | Mage Wars Complete Codex",
+			" Game Documents | Mage Wars Condition Markers",
+			" Game Documents | Spellbook Builder Install Help",
+			" Game Documents | ChangeLog",
+			]),
 		1100000: ("1.10.0.0", "03 September 2014", [
 			" Game Documents | Playing Mage Wars on OCTGN",
 			" Game Documents | OCTGN Apprentice Mode",
