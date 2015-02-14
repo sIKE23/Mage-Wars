fc74ae6f205af71b007861c126916ed021c211a0
ACG <anandaguneratne@gmail.com>
2015-02-13 19:07:37 -0600

    #171 - Implemented searchable codex
    #171 - Players can now search an E-codex using the 'Search Codex' group
    action. The codex currently only contains terms from the core set, and
    those terms have been copied and pasted verbatim. Long term, the entries
    will be tailored to have the most relevant information in an easily
    accessible package, including FAQs and clarifications. I also want to
    make it so that text entry accepts several different terms for each
    entry, (for instance, 'melee +X' and 'Melee +X' would both be valid,
    ideally.)

---
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/help.py
@@ -0,0 +1,26 @@
+############################################################################
+######################		Codex		    ########################
+############################################################################
+import sys
+sys.path.append(wd("lib"))
+import os
+
+def searchCodex(group, x=0, y=0):
+        textDirectory = os.path.split(os.path.dirname(__file__))[0]+'\{}'.format('scripts\scriptText')
+        rawCodex = list(open('{}\{}{}'.format(textDirectory,'Codex','.txt'),'r'))
+        codexDict = {}
+        entry = []
+        for line in rawCodex:
+                if line.replace('\n','').strip(' ') == '#':
+                        if len(entry) >= 2:
+                                entry[1] = ' '.join(entry[1:])
+                                codexDict[entry[0]] = entry[1]
+                        entry = []
+                else: entry.append(line.replace('\n','').strip(' '))
+        debug(str(codexDict))
+        while True:
+                term = askString('What would you like to know more about?','Enter codex term here')
+                if (codexDict.get(term) and
+                    (askChoice("{}:\n{}".format(term,codexDict.get(term)),
+                               ['Search for another term','Thanks, I\'m done'],
+                               ['#666699','#000000']) != 1)): break
