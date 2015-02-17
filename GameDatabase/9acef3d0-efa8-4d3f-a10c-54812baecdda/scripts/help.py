4d12b602caf1033b0c7453dab2a2cdb1ece8e278
ACG <anandaguneratne@gmail.com>
2015-02-16 19:07:01 -0600

    #171 - Searchable codex contains all terms
    #171 - Expanded the searchable codex to include every keyword in the
    game. Some work still needs to be done to add FAQ clarifications.
    
    #171 - Improved the codex searching interface so that players can break
    out of the loop without having to enter a correct keyword.
    
    #171 - Fixed formatting error on apostrophes and corrected some
    grammatical mistakes in the process.

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/help.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/help.py
@@ -11,16 +11,15 @@ def searchCodex(group, x=0, y=0):
         codexDict = {}
         entry = []
         for line in rawCodex:
-                if line.replace('\n','').strip(' ') == '#':
+                if line[0] == '#':
                         if len(entry) >= 2:
                                 entry[1] = ' '.join(entry[1:])
                                 codexDict[entry[0]] = entry[1]
                         entry = []
                 else: entry.append(line.replace('\n','').strip(' '))
-        debug(str(codexDict))
         while True:
                 term = askString('What would you like to know more about?','Enter codex term here')
-                if (codexDict.get(term) and
-                    (askChoice("{}:\n{}".format(term,codexDict.get(term)),
-                               ['Search for another term','Thanks, I\'m done'],
-                               ['#666699','#000000']) != 1)): break
+                if not (term): break
+                if (codexDict.get(term) and askChoice("{}:\n{}".format(term,codexDict.get(term)),
+                                                      ['Search for another term','Thanks, I\'m done'],
+                                                      ['#666699','#000000']) != 1): break
