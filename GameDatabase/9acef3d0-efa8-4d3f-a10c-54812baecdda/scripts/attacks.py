d7160cd079268d03ad2d78681e3e16c8f686ee87
sIKE23 <sike@pobox.com>
2015-10-17 13:20:44 -0500

    #304 - wounded plant creatures now buff bloodthirsty creatures

--- GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/attacks.py
+++ GameDatabase/9acef3d0-efa8-4d3f-a10c-54812baecdda/scripts/attacks.py
@@ -360,7 +360,6 @@ def getAdjustedDice(aTraitDict,attack,dTraitDict):
 				attackDice -= dTraitDict.get('Aegis',0)
 				attackDice += (aTraitDict.get('Bloodthirsty',0) if ((defender.markers[Damage] or (defender.Type=="Mage" and defender.controller.Damage))
 																	and (attacker and not hasAttackedThisTurn(attacker))
-																	and not 'Plant' in defender.subtype
 																	and defender.type in ['Creature','Mage']
 																	and not dTraitDict.get('Nonliving')) else 0)
 				attackDice += dTraitDict.get(attack.get('Type'),0) #Elemental weaknesses/resistances
